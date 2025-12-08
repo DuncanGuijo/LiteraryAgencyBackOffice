246e9e80ebeb409390cb9c0dbbb0fb8b364faba6
Duncan <duncan.guijo.fernandez@gmail.com>
2025-12-08 23:46:28 +0100

    Fix: delete dummy code

--- LiteraryAgencyBackoffice/routes/api.php
+++ LiteraryAgencyBackoffice/routes/api.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
-
 // Without Token
 Route::prefix('v1')->group(function () {
     Route::post('/register', [UserController::class, 'register']);
