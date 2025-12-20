3017e2838c3e33adb520d38e33147a0bed48827c
Duncan <duncan.guijo.fernandez@gmail.com>
2025-12-20 13:41:43 +0100

    Feat: Change endpoints naming with standar api rest naming

--- LiteraryAgencyBackoffice/routes/api.php
+++ LiteraryAgencyBackoffice/routes/api.php
@@ -16,30 +16,35 @@ Route::prefix('v1')->group(function () {
 
 // With Token
 Route::prefix('v1')->middleware(['auth:api'])->group(function () {
+
     // Users
-    Route::get('/user/{id}', [UserController::class, 'get']);
-    Route::get('/logout', [UserController::class,'logout']);
+    Route::get('/user/{id}', [UserController::class, 'get'])->whereNumber('id');
+    Route::get('/logout', [UserController::class, 'logout']);
+
     // Books
-    Route::post('createBook',  [BookController::class,'create']);
-    Route::post('updateBook',  [BookController::class,'update']);
-    Route::post('destroyBook',  [BookController::class,'destroy']);
-    Route::get('showBook', [BookController::class, 'show']);
-    Route::get('tableBooks',[BookController::class, 'index']);
-    //Agencies
-    Route::post('createAgency',  [AgencyController::class,'create']);
-    Route::post('updateAgency',  [AgencyController::class,'update']);
-    Route::post('destroyAgency',  [AgencyController::class,'destroy']);
-    Route::get('showAgency', [AgencyController::class, 'show']);
-    Route::get('tableAgencies',[AgencyController::class, 'index']);
-    //Authors
-    Route::post('createAuthor',  [AuthorController::class,'create']);
-    Route::post('updateAuthor',  [AuthorController::class,'update']);
-    Route::post('destroyAuthor',  [AuthorController::class,'destroy']);
-    Route::get('showAuthor', [AuthorController::class, 'show']);
-    Route::get('tableAuthors',[AuthorController::class, 'index']);
-    //Genres
-    Route::post('createGenre',  [GenreController::class,'create']);
-    Route::post('updateGenre',  [GenreController::class,'update']);
-    Route::post('destroyGenre',  [GenreController::class,'destroy']);
-    Route::get('tableGenres',[GenreController::class, 'index']); 
+    Route::get('/books', [BookController::class, 'index']);
+    Route::get('/books/{id}', [BookController::class, 'show'])->whereNumber('id');
+    Route::post('/books', [BookController::class, 'create']);
+    Route::put('/books/{id}', [BookController::class, 'update'])->whereNumber('id');
+    Route::delete('/books/{id}', [BookController::class, 'destroy'])->whereNumber('id');
+
+    // Agencies
+    Route::get('/agencies', [AgencyController::class, 'index']);
+    Route::get('/agencies/{id}', [AgencyController::class, 'show'])->whereNumber('id');
+    Route::post('/agencies', [AgencyController::class, 'create']);
+    Route::put('/agencies/{id}', [AgencyController::class, 'update'])->whereNumber('id');
+    Route::delete('/agencies/{id}', [AgencyController::class, 'destroy'])->whereNumber('id');
+
+    // Authors
+    Route::get('/authors', [AuthorController::class, 'index']);
+    Route::get('/authors/{id}', [AuthorController::class, 'show'])->whereNumber('id');
+    Route::post('/authors', [AuthorController::class, 'create']);
+    Route::put('/authors/{id}', [AuthorController::class, 'update'])->whereNumber('id');
+    Route::delete('/authors/{id}', [AuthorController::class, 'destroy'])->whereNumber('id');
+
+    // Genres
+    Route::get('/genres', [GenreController::class, 'index']);
+    Route::post('/genres', [GenreController::class, 'create']);
+    Route::put('/genres/{id}', [GenreController::class, 'update'])->whereNumber('id');
+    Route::delete('/genres/{id}', [GenreController::class, 'destroy'])->whereNumber('id');
 });
